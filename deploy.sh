5d42f733b6ef65835d677fd03694658525834842
keneleven <nattawut.eleven@mail.kmutt.ac.th>
2019-08-18 23:20:07 +0700

    update code

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 git push -f https://github.com/keneleven/keneleven.github.io master
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
